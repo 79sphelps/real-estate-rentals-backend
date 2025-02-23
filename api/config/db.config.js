e152b6580b3a4e858778c6dc20ede6137f6ca8bb
Steve Phelps <79sphelps@gmail.com>
2025-02-23 10:19:32 -0800

    Updating local MongoDB ref

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // url: 'mongodb://localhost:27017/northOneToDos'
+  // "mongodb://localhost:27017/phelpsRentals",
   url: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.po6aaqm.mongodb.net/phelpsRentals?retryWrites=true&w=majority`
 };

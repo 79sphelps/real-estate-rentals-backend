514cd3ffa2d206f1d64b6152f515e792d4a6eac8
Steve Phelps <79sphelps@gmail.com>
2025-10-02 08:46:01 -0700

    Adding missing axios package

---
+++ cron-standalone.js
@@ -0,0 +1,19 @@
+const cron = require('cron');
+const https = require('https');
+
+const backendUrl = 'https://real-estate-rentals-backend.onrender.com/';
+const job = new cron.CronJob('*/14 * * * *', () => {
+    // this will be executed every 14 minutes
+  https.get(backendUrl, (res) => {
+    console.log(`Backend pinged successfully with status code: ${res.statusCode}`);
+    if (res.statusCode === 200) {
+        console.log('Server restarted successfully');
+    } else {
+        console.log('Failed to restart the server with status code:', res.statusCode);
+    }
+  }).on('error', (e) => {
+    console.error(`Error during restart: ${e.message}`);
+  });
+});
+
+job.start();
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const axios = require('axios');
 const cronJob = require('./cron').job;
 const dotenv = require("dotenv");
 dotenv.config();
